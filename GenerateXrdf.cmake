5f833327bec8f37922b040d63b57755295f847b2
Arturo Laurenzi <arturo.laurenzi@iit.it>
2022-01-17 15:06:09 +0100

    refactor && docs

--- GenerateXrdf.cmake
+++ GenerateXrdf.cmake
@@ -1,3 +1,21 @@
+# create main targets 
+if(NOT TARGET generate)
+    add_custom_target(generate)
+endif()
+
+if(NOT TARGET publish)
+    add_custom_target(publish)
+endif()
+
+# generate warning if plain make is invoked
+add_custom_target(print_usage
+    ALL
+    COMMAND echo 'Note \(for developers\): use \"make generate\" to generate urdf and srdf to the build tree, \"make publish\" to copy the results to the source tree'
+)
+
+# function to generate urdf and srdf from xacros; this is an internal
+# function which is not supposed to be called directly: use generate_urdf, 
+# generate_srdf
 function(generate_xrdf type)
 
     cmake_parse_arguments(GEN_XRDF "ADD_TO_ALL;GEN_ACM" "XACRO;CONFIG" "" ${ARGN})
@@ -8,10 +26,23 @@ function(generate_xrdf type)
 
     set(URDF_DIR ${CMAKE_SOURCE_DIR}/${ROBOT_NAME}_urdf/urdf)
     set(SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/${type})
-    set(CONFIG_ABSPATH ${URDF_DIR}/${GEN_XRDF_CONFIG})
-    set(XACRO_ABSPATH ${SRC_DIR}/${GEN_XRDF_XACRO})
+
+    if(IS_ABSOLUTE ${GEN_XRDF_CONFIG})
+        set(CONFIG_ABSPATH ${GEN_XRDF_CONFIG})
+    else()
+        set(CONFIG_ABSPATH ${URDF_DIR}/${GEN_XRDF_CONFIG})
+    endif()
+
+    if(IS_ABSOLUTE ${GEN_XRDF_XACRO})
+        set(XACRO_ABSPATH ${GEN_XRDF_CONFIG})
+    else()
+        set(XACRO_ABSPATH ${SRC_DIR}/${GEN_XRDF_XACRO})
+    endif()
+
+    # path to the urdf file (used only if type == srdf to compute acm)
     set(URDF_FILE ${CMAKE_BINARY_DIR}/${ROBOT_NAME}_urdf/${CONFIG_NAME}.urdf)
 
+    # files that the generated xrdf depends upon
     file(GLOB_RECURSE XACRO_FILES ${SRC_DIR}/*.${type}.xacro)
     file(GLOB_RECURSE CONFIG_FILES ${URDF_DIR}/config/*.urdf.xacro)
     
@@ -20,20 +51,22 @@ function(generate_xrdf type)
 
     # conditional generation of acm
     if(${GEN_XRDF_GEN_ACM})
-        set(GEN_ACM_DEPENDS "${URDF_FILE}")
+        set(GEN_ACM_DEPENDS generate_urdf_${CONFIG_NAME})
         set(GEN_ACM_CMD moveit_compute_default_collisions --urdf_path ${URDF_FILE} --srdf_path ${OUTPUT_FILE})
     endif()
 
+    # target and command to perform the actual generation
     add_custom_target(generate_${type}_${CONFIG_NAME}
-        DEPENDS ${OUTPUT_FILE})
+        DEPENDS ${OUTPUT_FILE} ${GEN_ACM_DEPENDS})
 
     add_custom_command(
         OUTPUT ${OUTPUT_FILE}
         COMMENT "Generating ${OUTPUT_FILE} .."
         COMMAND rosrun xacro xacro ${XACRO_ABSPATH} config:=${CONFIG_ABSPATH} -o ${OUTPUT_FILE}
         COMMAND ${GEN_ACM_CMD}
-        DEPENDS ${XACRO_FILES} ${CONFIG_FILES} ${GEN_ACM_DEPENDS}
+        DEPENDS ${XACRO_FILES} ${CONFIG_FILES} 
     )
+    message(STATUS "custom cmd with output=${OUTPUT_FILE}, depends=${GEN_ACM_DEPENDS}")
 
     # publish to source folder
     add_custom_target(publish_${type}_${CONFIG_NAME}
@@ -43,29 +76,66 @@ function(generate_xrdf type)
 
     add_dependencies(publish_${type}_${CONFIG_NAME} generate_${type}_${CONFIG_NAME})
 
+    # add to main targets
     if(${GEN_XRDF_ADD_TO_ALL})
-        if(NOT TARGET generate_${type}_all)
-            add_custom_target(generate_${type}_all ALL)
-        endif()
-        if(NOT TARGET publish_${type}_all)
-            add_custom_target(publish_${type}_all)
-        endif()
-        add_dependencies(publish_${type}_all publish_${type}_${CONFIG_NAME})
-        add_dependencies(generate_${type}_all generate_${type}_${CONFIG_NAME})
+        add_dependencies(publish publish_${type}_${CONFIG_NAME})
+        add_dependencies(generate generate_${type}_${CONFIG_NAME})
     endif()
 
 endfunction()
 
 
+# generate_urdf
+# Generate the urdf from a xacro file, with a given configuration file folding
+# variables to customize the generation
+# 
+# Usage:
+#   generate_urdf(
+#     XACRO <path-to-main-xacro-file>     (if not absolute, base directory is ${ROBOT_NAME}_urdf/urdf)
+#     CONFIG <path-to-config-xacro-file>  (if not absolute, base directory is ${ROBOT_NAME}_urdf/urdf)
+#     [ADD_TO_ALL TRUE|FALSE]
+#   ) 
+#
+# If ADD_TO_ALL is enabled, this urdf file is generated/published with the default target generate, publish.
+# 
 function(generate_urdf)
     generate_xrdf(urdf ${ARGV})
 endfunction()
 
+
+# generate_srdf
+# Generate the srdf from a xacro file, with a given configuration file folding
+# variables to customize the generation. Optionally, this method also generates
+# an Allowed Collision Matrix (ACM) in the form of <disable_collision> srdf tags.
+# 
+# Usage:
+#   generate_srdf(
+#     XACRO <path-to-main-xacro-file>     (if not absolute, base directory is ${ROBOT_NAME}_srdf/srdf)
+#     CONFIG <path-to-config-xacro-file>  (if not absolute, base directory is ${ROBOT_NAME}_urdf/urdf)
+#     [ADD_TO_ALL FALSE]
+#     [GEN_ACM FALSE]
+#   ) 
+#
+# If ADD_TO_ALL is enabled, this urdf file is generated/published with the default target generate, publish.
+# 
 function(generate_srdf)
     generate_xrdf(srdf ${ARGV})
-
 endfunction()
 
+
+# generate_capsule_urdf
+# Generate the capsule collisin model from a generated urdf.
+# 
+# Usage:
+#   generate_capsule_urdf(
+#     CONFIG_NAME <config-xacro-name> 
+#     [ADD_TO_ALL TRUE|FALSE]
+#   ) 
+#
+# Note that CONFIG_NAME is the basename of the xacro config file that was used to generate the urdf,
+# for which the capsule model is to be generated.
+# If ADD_TO_ALL is enabled, this urdf file is generated/published with the default target generate, publish.
+#
 function(generate_capsule_urdf)
     
     cmake_parse_arguments(GEN_CAPSULE "ADD_TO_ALL" "CONFIG_NAME" "" ${ARGN})
@@ -74,12 +144,8 @@ function(generate_capsule_urdf)
     set(SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/urdf)
     set(GEN_CAPSULE_URDF ${CMAKE_CURRENT_BINARY_DIR}/${CONFIG_NAME}.urdf)
     set(CAPSULE_FOLDER ${CMAKE_CURRENT_BINARY_DIR}/capsules)
-    if(${GEN_CAPSULE_ADD_TO_ALL})
-        set(ALL_OPT ALL)
-    endif()
 
     add_custom_target(generate_urdf_${CONFIG_NAME}_capsule
-        ${ALL_OPT}
         DEPENDS ${OUTPUT_FILE}
         DEPENDS generate_urdf_${CONFIG_NAME}
     )
@@ -100,11 +166,25 @@ function(generate_capsule_urdf)
     )
 
     if(${GEN_CAPSULE_ADD_TO_ALL})
-        add_dependencies(publish_urdf_all publish_urdf_${CONFIG_NAME}_capsule)
+        add_dependencies(generate generate_urdf_${CONFIG_NAME}_capsule)
+        add_dependencies(publish publish_urdf_${CONFIG_NAME}_capsule)
     endif()
 
 endfunction()
 
+
+# generate_capsule_srdf_acm
+# Generate the Allowed Collision Matrix (ACM) in the form of <disable_collision> srdf tags,
+# corresponding to the capsule urdf based on the provided config file.
+# 
+# Usage:
+#   generate_capsule_srdf_acm(
+#     CONFIG_NAME <config-xacro-name> 
+#     [ADD_TO_ALL TRUE|FALSE]
+#   ) 
+#
+# If ADD_TO_ALL is enabled, this urdf file is generated/published with the default target generate, publish.
+# 
 function(generate_capsule_srdf_acm)
     
     cmake_parse_arguments(GEN_CAPSULE "ADD_TO_ALL" "CONFIG_NAME" "" ${ARGN})
@@ -114,12 +194,8 @@ function(generate_capsule_srdf_acm)
     set(URDF_FILE ${CMAKE_BINARY_DIR}/${ROBOT_NAME}_urdf/${CONFIG_NAME}_capsule.urdf)
     set(SRDF_FILE ${CMAKE_CURRENT_BINARY_DIR}/${CONFIG_NAME}.srdf)
     set(SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/srdf)
-    if(${GEN_CAPSULE_ADD_TO_ALL})
-        set(ALL_OPTION ALL)
-    endif()
 
     add_custom_target(generate_srdf_${CONFIG_NAME}_capsule
-        ${ALL_OPTION}
         DEPENDS ${OUTPUT_FILE}
     )
 
@@ -139,7 +215,8 @@ function(generate_capsule_srdf_acm)
     )
 
     if(${GEN_CAPSULE_ADD_TO_ALL})
-        add_dependencies(publish_srdf_all publish_srdf_${CONFIG_NAME}_capsule)
+        add_dependencies(generate generate_srdf_${CONFIG_NAME}_capsule)
+        add_dependencies(publish publish_srdf_${CONFIG_NAME}_capsule)
     endif()
 
 endfunction()
